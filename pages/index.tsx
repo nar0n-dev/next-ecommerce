2574ab5e2b3dfd37b0200383938528ab425c395d
Natan Cardoso <natanscardoso@outlook.com>
2022-06-25 17:04:14 -0300

    Descrições card

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,22 +13,22 @@ const IndexPage = () => {
         <div className="container">
           <article style={{ backgroundImage: 'url(/images/featured-1.jpg)' }} className="featured-item featured-item-large">
             <div className="featured-item__content">
-              <h3>New arrivals are now in!</h3>
-              <a href="#" className="btn btn--rounded">Ver Mais</a>
+              <h3>Novidades!</h3>
+              <a href="products" className="btn btn--rounded">Ver Mais</a>
             </div>
           </article>
 
           <article style={{ backgroundImage: 'url(/images/featured-2.jpg)' }} className="featured-item featured-item-small-first">
             <div className="featured-item__content">
-              <h3>Basic t-shirts $29,99</h3>
-              <a href="#" className="btn btn--rounded">Ver Mais</a>
+              <h3>Calças a partir de R$74,99</h3>
+              <a href="products" className="btn btn--rounded">Ver Mais</a>
             </div>
           </article>
 
           <article style={{ backgroundImage: 'url(/images/featured-3.jpg)' }} className="featured-item featured-item-small">
             <div className="featured-item__content">
-              <h3>Sale this summer</h3>
-              <a href="#" className="btn btn--rounded">Ver Mais</a>
+              <h3>Ofertas de Inverno</h3>
+              <a href="products" className="btn btn--rounded">Ver Mais</a>
             </div>
           </article>
         </div>
